c4d3f78be55e6a6cce5f19fd1e75cf4dd2f32726
deepak kumar jha <deepak.cse.jha@gmail.com>
2024-09-20 16:18:13 +0530

    card is remaining

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-app.controller("MainController", function ($scope, $mdSidenav) {
-  arr = ["deepak", "ayush"];
+app.controller("MainController", function ($scope) {
+  $scope.arr = [
+    { room:"DELUXE SUITE", details: "5ROOMS | BO#1 | 10#2 | MO#4| TC#2| ЕР, CP|"},
+    { room: "SUPER DELUXE", details: "10 ROOMS | BO#1 | 10#2 | MO#4 | TC#2 | EP, CP|" },
+    { room: "LOTUS SUITE", details: "10 ROOMS | BO#1 | 10#2 | MO#4 | TC#2 | ЕР, CP|" },
+    {room:"PREMIUM SUITE",details:"10 ROOMS | BO#1 | 10#2 | MO#4 | TC#2 | ЕР, CP|"},
+    {room:"EXECUTIVE SUITE",details:"10 ROOMS | BO#1 | 10#2 | MO#4 | TC#2 | ЕР, CP|"},
+  ];
+  $scope.searchText="";
+
+  $scope.activeSpan = null;
+
+  // This function is called when a span is clicked
+  $scope.setActiveSpan = function(spanNumber) {
+    // Set activeSpan to the spanNumber (1, 2, or 3) that was clicked
+    $scope.activeSpan = spanNumber;
+  };
 });
